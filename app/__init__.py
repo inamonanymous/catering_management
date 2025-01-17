20836b5ecdc0504e44d451f2caa763094409fa1c
Stephen Aguilar <stephenonline25@gmail.com>
2025-01-17 16:22:58 +0800

    appconfig

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, session
+from flask import Flask
 from app.main import main
-
+from app.config import AppConfig
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main)
+    app.config.from_object(AppConfig)
     return app
