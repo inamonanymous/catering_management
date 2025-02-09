2208124aff8ca428f63185146d3c7b1c41b8b229
Francis Cabusas <cabusasfg779@gmail.com>
2025-02-09 15:38:56 +0800

    Update Script

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,54 @@
 $(document).ready(() => {
-  $(".mobile-btn").on("click", function () {
-    $(".mobile-navbar").slideToggle();
+  const html = $("html"),
+    themeIcon = $("#themeIcon"),
+    theme = localStorage.getItem("theme");
+
+  theme == "dark" ? enableDarkMode() : disableDarkMode();
+
+  $(document).on("click", ".changeTheme", () =>
+    html.attr("data-theme") == "dark" ? disableDarkMode() : enableDarkMode()
+  );
+
+  function enableDarkMode() {
+    html.attr("data-theme", "dark");
+    themeIcon.removeClass("bi-moon").addClass("bi-sun");
+    localStorage.setItem("theme", "dark");
+  }
+
+  function disableDarkMode() {
+    html.attr("data-theme", "light");
+    themeIcon.removeClass("bi-sun").addClass("bi-moon");
+    localStorage.setItem("theme", "light");
+  }
+
+  const sidebar = $(".sidebar"),
+    activeLink = window.location.href.split("?")[0],
+    menuLink = $(".menu-link").filter(function () {
+      return $(this).attr("href") == activeLink;
+    });
+  console.log(menuLink);
+  if (menuLink.length) {
+    menuLink.addClass("active-link");
+    const subMenu = menuLink.closest(".sub-menu");
+
+    if (subMenu.length)
+      subMenu
+        .addClass("active")
+        .prev(".sub-btn")
+        .find(".dropdown")
+        .addClass("rotate");
+  }
+
+  $(window).resize(() => {
+    if (!$("#btn-sidebar-mobile").is(":visible")) sidebar.removeClass("active");
+  });
+
+  $(document).on("click", function (event) {
+    if (
+      event.target.id === "btn-sidebar-mobile" ||
+      event.target.id === "btn-sidebar-close"
+    ) {
+      sidebar.toggleClass("active");
+    }
   });
 });
